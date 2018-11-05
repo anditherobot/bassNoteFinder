d049cc4e55e1a3c42d993257849af611ba81d3a1
Andi Milhomme <mandirosa3@gmail.com>
2018-11-04 22:07:05 -0500

    commencing angular 6 migration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { BassComponent } from './components/bass/bass.component';
+const routes: Routes = [{
+  path: 'bass',
+  component: BassComponent,
+},
+{ path: '',
+  redirectTo: '/bass',
+  pathMatch: 'full'
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BassComponent } from './components/bass/bass.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BassComponent
   ],
   imports: [
     BrowserModule,
