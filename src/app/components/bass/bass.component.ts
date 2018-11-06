93bb2fddd171ee3ca4d71950379da13d8c7564a2
Andi Milhomme <mandirosa3@gmail.com>
2018-11-06 00:55:59 -0500

    progress with hightlight, using fewer angular centric commands

--- src/app/components/bass/bass.component.ts
+++ src/app/components/bass/bass.component.ts
@@ -14,26 +14,45 @@ dString = ['d', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd']
 aString = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a'];
 eString = ['e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e'];
 
+displayNotesonE;
+displayNotesonA;
+displayNotesonD;
+displayNotesonG;
+
 selected: string;
 
   constructor() { }
 
   ngOnInit() {
   }
+
   showOnFret (selectednote) {
   this.selected = selectednote;
+    console.log('get note', selectednote);
 
-    console.log('get note', selectednote)
-      
-    // select(index);
 
-    // this.displayNotesonE =  highlight(_note, this.eString );
-    // this.displayNotesonA =  highlight(_note, this.aString );
-    // this.displayNotesonD =  highlight(_note, this.dString );
-    // this.displayNotesonG =  highlight(_note, this.gString );
-   
+     this.displayNotesonE =  this.highlight(selectednote, this.eString );
+     this.displayNotesonA =  this.highlight(selectednote, this.aString );
+     this.displayNotesonD =  this.highlight(selectednote, this.dString );
+     this.displayNotesonG =  this.highlight(selectednote, this.gString );
+
   };
 
+
+  //TODO: refactor with functional programming like find(), filter or something. try not 
+  // to do it without loops. 
+    
+  highlight(selection, array) {
+      let indices = [];
+      for(let i= 0; i < array.length; i++){
+        if(array[i] === selection) {
+          indices.push(selection);
+        }
+      }
+      console.log(this.displayNotesonA);
+      console.log('indices', indices);
+      return indices;
+   }
   
 }
 
