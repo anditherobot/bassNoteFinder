0ba211bd979242c674c8ad0480671da959d8cf9d
Andi Milhomme <mandirosa3@gmail.com>
2018-11-06 00:36:35 -0500

    Fixed the elements moving on selection

--- src/app/components/bass/bass.component.ts
+++ src/app/components/bass/bass.component.ts
@@ -6,16 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bass.component.scss']
 })
 export class BassComponent implements OnInit {
- notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-fretNumber = ['O', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+ notes = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
+fretNumber = ['Open', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
 
 gString = ['g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g'];
 dString = ['d', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd'];
 aString = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a'];
 eString = ['e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e'];
+
+selected: string;
+
   constructor() { }
 
   ngOnInit() {
   }
+  showOnFret (selectednote) {
+  this.selected = selectednote;
 
+    console.log('get note', selectednote)
+      
+    // select(index);
+
+    // this.displayNotesonE =  highlight(_note, this.eString );
+    // this.displayNotesonA =  highlight(_note, this.aString );
+    // this.displayNotesonD =  highlight(_note, this.dString );
+    // this.displayNotesonG =  highlight(_note, this.gString );
+   
+  };
+
+  
 }
+
+
