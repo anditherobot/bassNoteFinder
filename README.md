ded6563cd2a25345bf151d1193f61a86ef2a97be
Andi Milhomme <mandirosa3@gmail.com>
2019-03-25 21:22:55 -0400

    updated the readme

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
 # Bassngx
+This is my coolest project so far. 
+It useful and still in the experimental phase. 
+Most of the challenges are UX based. 
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.2.
+How can I make this usable
+## Ideas
+* Voice commands
+* Keyboard shortcuts?
 
+
+
+
+## Technical
+------------------------------------
 ## Development server
+This is an Angular application, it uses angular 
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.2.
+You will also need to have nodejs/ npm installed on your machine
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/components/bass/bass.component.ts
+++ src/app/components/bass/bass.component.ts
@@ -7,91 +7,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BassComponent implements OnInit {
 
-fretNumber = ['Open', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
-notes;
-gString;
-dString;
-aString;
-eString;
+  fretNumber = ['Open', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+  notes;
+  gString;
+  dString;
+  aString;
+  eString;
 
-gStringSharp = ['g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g'];
-dStringSharp = ['d', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd'];
-aStringSharp = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a'];
-eStringSharp = ['e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e'];
+  gStringSharp = ['g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g'];
+  dStringSharp = ['d', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd'];
+  aStringSharp = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a'];
+  eStringSharp = ['e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e'];
 
-gStringFlat = ['g', 'a&#9837;', 'a', 'c&#9837;', 'c&#9837;', 'c', 'd&#9837;', 'd', 'f&#9837;', 'f&#9837;', 'f', 'g&#9837;', 'g'];
-dStringFlat = ['d', 'f&#9837;', 'f&#9837;', 'f', 'g&#9837;', 'g', 'a&#9837;', 'a', 'c&#9837;', 'c&#9837;', 'c', 'd&#9837;', 'd'];
-aStringFlat = ['a', 'c&#9837;', 'c&#9837;', 'c', 'd&#9837;', 'd', 'f&#9837;', 'f&#9837;', 'f', 'g&#9837;', 'g', 'a&#9837;', 'a'];
-eStringFlat = ['f&#9837;', 'f', 'g&#9837;', 'g', 'a&#9837;', 'a', 'c&#9837;', 'c&#9837;', 'c', 'd&#9837;', 'd', 'f&#9837;', 'f&#9837;'];
+  gStringFlat = ['g', 'a&#9837;', 'a', 'c&#9837;', 'c&#9837;', 'c', 'd&#9837;', 'd', 'f&#9837;', 'f&#9837;', 'f', 'g&#9837;', 'g'];
+  dStringFlat = ['d', 'f&#9837;', 'f&#9837;', 'f', 'g&#9837;', 'g', 'a&#9837;', 'a', 'c&#9837;', 'c&#9837;', 'c', 'd&#9837;', 'd'];
+  aStringFlat = ['a', 'c&#9837;', 'c&#9837;', 'c', 'd&#9837;', 'd', 'f&#9837;', 'f&#9837;', 'f', 'g&#9837;', 'g', 'a&#9837;', 'a'];
+  eStringFlat = ['f&#9837;', 'f', 'g&#9837;', 'g', 'a&#9837;', 'a', 'c&#9837;', 'c&#9837;', 'c', 'd&#9837;', 'd', 'f&#9837;', 'f&#9837;'];
 
 
 
-displayNotesonE;
-displayNotesonA;
-displayNotesonD;
-displayNotesonG;
+  displayNotesonE;
+  displayNotesonA;
+  displayNotesonD;
+  displayNotesonG;
 
-selected: string;
-
+  selected: string;
+  author = { id: 1, name: 'andi', lastName: 'milhomme' };
   constructor() { }
 
   ngOnInit() {
     //Keeping it sharp by default.
     this.notes = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
 
-    this.gString = this.gStringSharp ;
-    this.dString = this.dStringSharp ;
-    this.aString = this.aStringSharp ;
-    this.eString = this.eStringSharp ;
+    this.gString = this.gStringSharp;
+    this.dString = this.dStringSharp;
+    this.aString = this.aStringSharp;
+    this.eString = this.eStringSharp;
   }
 
-  showOnFret (selectednote) {
-  this.selected = selectednote;
+  showOnFret(selectednote) {
+    this.selected = selectednote;
     console.log('get note', selectednote);
 
 
-     this.displayNotesonE =  this.highlight(selectednote, this.eString );
-     this.displayNotesonA =  this.highlight(selectednote, this.aString );
-     this.displayNotesonD =  this.highlight(selectednote, this.dString );
-     this.displayNotesonG =  this.highlight(selectednote, this.gString );
+    this.displayNotesonE = this.highlight(selectednote, this.eString);
+    this.displayNotesonA = this.highlight(selectednote, this.aString);
+    this.displayNotesonD = this.highlight(selectednote, this.dString);
+    this.displayNotesonG = this.highlight(selectednote, this.gString);
 
   };
 
 
-  toggleType(type){
-if(type == NoteType.Flat){
+  toggleType(type) {
+    if (type == NoteType.Flat) {
 
-  this.notes = ["c", "d&#9837;", "d", "f&#9837;", "f&#9837;", "f", "g&#9837;", "g", "a&#9837;", "a", "c&#9837;", "c&#9837;"];
-  this.gString = this.gStringFlat ;
-  this.dString = this.dStringFlat ;
-  this.aString = this.aStringFlat ;
-  this.eString = this.eStringFlat ;
-}
-else if(type == NoteType.Sharp) {
-  this.notes = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
-  this.gString = this.gStringSharp ;
-  this.dString = this.dStringSharp ;
-  this.aString = this.aStringSharp ;
-  this.eString = this.eStringSharp ;
-}
+      this.notes = ["c", "d&#9837;", "d", "f&#9837;", "f&#9837;", "f", "g&#9837;", "g", "a&#9837;", "a", "c&#9837;"];
+      this.gString = this.gStringFlat;
+      this.dString = this.dStringFlat;
+      this.aString = this.aStringFlat;
+      this.eString = this.eStringFlat;
+    }
+    else if (type == NoteType.Sharp) {
+      this.notes = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
+      this.gString = this.gStringSharp;
+      this.dString = this.dStringSharp;
+      this.aString = this.aStringSharp;
+      this.eString = this.eStringSharp;
+    }
   }
   //TODO: refactor with functional programming like find(), filter or something. try not 
   // to do it without loops. 
-    
+
   highlight(selection, array) {
-      let indices = [];
-      for(let i= 0; i < array.length; i++){
-        if(array[i] === selection) {
-          indices.push(selection);
-        }
+    let indices = [];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === selection) {
+        indices.push(selection);
       }
-      return indices;
-   }
-  
+    }
+    return indices;
+  }
+
+
+
+
 }
+  
+
 
 enum NoteType {
-  Sharp = 0, 
-  Flat = 1
+  Sharp = 0,
+  Flat = 1,
+  Natural = 3
 }
 
