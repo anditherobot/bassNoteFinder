95dc1eebcdc70df5d4e2c338297f01d852ce64a9
Andi Milhomme <mandirosa3@gmail.com>
2019-05-05 13:08:10 -0400

    added a more accurate counter, added reset button

--- src/app/components/sequencer/sequencer.component.ts
+++ src/app/components/sequencer/sequencer.component.ts
@@ -1,48 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { startTimeRange } from '@angular/core/src/profile/wtf_impl';
+import { Component, OnInit } from "@angular/core";
+import { startTimeRange } from "@angular/core/src/profile/wtf_impl";
 
 @Component({
-  selector: 'app-sequencer',
-  templateUrl: './sequencer.component.html',
-  styleUrls: ['./sequencer.component.scss']
+  selector: "app-sequencer",
+  templateUrl: "./sequencer.component.html",
+  styleUrls: ["./sequencer.component.scss"]
 })
 export class SequencerComponent implements OnInit {
- 
-    _notes :Array<String>;
-    activeFlag = [true, true, true, true];
+  _notes: Array<String>;
+  activeFlag = [true, true, true, true];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this._notes = ["C", "F", "G", "C"];
-  
+
     console.log(this._notes);
   }
 
- timers;
+  timer = this.activeFlag.length;
   counter = 0;
   flagTrack = undefined;
-startTime(time){
+  startTime(time) {
+    if (time > 0) {
+      this.timer = time;
+      this.activeFlag[this.counter] = false;
+      setTimeout(() => {
+       
+        console.log("time init", this.activeFlag);
+        this.counter++;
+        this.flagTrack = this.activeFlag;
+        this.startTime(time - 1);
+      }, 1500);
+    } else {
+      this.counter = 0;
+    }
+  }
 
-if(time > 0) {
-
-  this.timers = time;
-setTimeout(()=>{
-
-
-
-  this.activeFlag[this.counter ] =  !this.activeFlag[this.counter ] ;
-  console.log('time init', this.activeFlag);
-  this.counter ++;
-  this.flagTrack = this.activeFlag;
-  this.startTime(time - 1)
-}, 1500);
-
-}
-else {
-  this.counter = 0;
-}
+  reset(){
+    this.activeFlag = [true, true, true, true];
+    this.timer =  this.activeFlag.length;
+  }
 
 
-}
 }
